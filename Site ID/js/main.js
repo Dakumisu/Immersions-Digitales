cb4f7e8e561965ef5637e1b3c4a05dbf323be9df
Thoma Lecornu <thoma.lecornu@gmail.com>
2020-11-03 14:18:40 +0100

    Update main.js

--- Site ID/js/main.js
+++ Site ID/js/main.js
@@ -2,7 +2,7 @@
 var scene = new THREE.Scene();
 
 ////////// CAMERA //////////
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); // CRÉATION DE LA CAMÉRA
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 3;
 
 /////// RENDERER ///////
