24d60b5ea27d7efdb47bfdc639fc3a7c7fc7fa89
Dakumisu <alex.gattefosse@gmail.com>
2020-11-08 04:57:32 +0100

    nik

--- Site ID/js/app.js
+++ Site ID/js/app.js
@@ -28,7 +28,6 @@ window.addEventListener('resize', () => {
 
 })
 
-
 /////// CENTRAL MODEL ///////
 var geometry = new THREE.BoxGeometry(.5, 3, 1);
 var material = new THREE.MeshLambertMaterial({ color: 0x0142AC });
@@ -50,41 +49,43 @@ render();
 var plane = new THREE.PlaneGeometry(1.6 / 1.5, .9 / 1.5);
 
 /////// PLANES MATERIALS ///////
-var materialPlane1 = new THREE.ShaderMaterial({
+
+var uniforms = {
+    texture1: { type: "t", value: THREE.ImageUtils.loadTexture( "./assets/img/atelier1.png" ) }
+};
+
+var materialPlane1 = new THREE.MeshLambertMaterial({
     vertexShader,
     fragmentShader,
-    uniforms: {
-      uTime: { value: 0.0 }
-    },
-    map: new THREE.TextureLoader().load('/assets/img/atelier1.png'),
+    uniforms: uniforms,
     side: THREE.DoubleSide
 });
 var materialPlane2 = new THREE.MeshLambertMaterial({
-    map: new THREE.TextureLoader().load('/assets/img/atelier2.png'),
+    map: new THREE.TextureLoader().load('./assets/img/atelier2.png'),
     side: THREE.DoubleSide
 });
 var materialPlane3 = new THREE.MeshLambertMaterial({
-    map: new THREE.TextureLoader().load('/assets/img/atelier3.png'),
+    map: new THREE.TextureLoader().load('./assets/img/atelier3.png'),
     side: THREE.DoubleSide
 });
 var materialPlane4 = new THREE.MeshLambertMaterial({
-    map: new THREE.TextureLoader().load('/assets/img/atelier4.png'),
+    map: new THREE.TextureLoader().load('./assets/img/atelier4.png'),
     side: THREE.DoubleSide
 });
 var materialPlane5 = new THREE.MeshLambertMaterial({
-    map: new THREE.TextureLoader().load('/assets/img/atelier5.png'),
+    map: new THREE.TextureLoader().load('./assets/img/atelier5.png'),
     side: THREE.DoubleSide
 });
 var materialPlane6 = new THREE.MeshLambertMaterial({
-    map: new THREE.TextureLoader().load('/assets/img/atelier6.png'),
+    map: new THREE.TextureLoader().load('./assets/img/atelier6.png'),
     side: THREE.DoubleSide
 });
 var materialPlane7 = new THREE.MeshLambertMaterial({
-    map: new THREE.TextureLoader().load('/assets/img/atelier7.png'),
+    map: new THREE.TextureLoader().load('./assets/img/atelier7.png'),
     side: THREE.DoubleSide
 });
 var materialPlane8 = new THREE.MeshLambertMaterial({
-    map: new THREE.TextureLoader().load('/assets/img/atelier8.png'),
+    map: new THREE.TextureLoader().load('./assets/img/atelier8.png'),
     side: THREE.DoubleSide
 });
 
